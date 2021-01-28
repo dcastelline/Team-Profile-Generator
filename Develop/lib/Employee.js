dbaa36969ce3d3ae97fb720d15d8e185ee02c201
Daniel <daniel.castelline@gmail.com>
2021-01-27 18:01:39 -0600

    defined each employee type, used class extends

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        console.log(`${data.name}`);
+    }
+    getID() {
+        console.log(`${data.id}`);
+    }
+    getEmail() {
+        console.log(`${data.email}`);
+    }
+    getRole() {
+        console.log(`${data.role}`);
+    }
+}
+
+module.exports = Employee;
